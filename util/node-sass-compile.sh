0938ab6d2284f9f00af0c54d0af8870abff27298
Henryk Kurze <hk@4so.de>
2017-01-03 15:12:23 +0100

    moved sass scripts to the package.json

--- util/node-sass-compile.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd ..
-cd src
-node-sass --output css/ --source-map true --source-map-contents sass/
--- util/node-sass-watch.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd ..
-cd src
-node-sass --watch --recursive --output css/ --source-map true --source-map-contents sass/
