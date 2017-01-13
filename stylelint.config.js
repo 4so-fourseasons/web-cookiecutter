e80aabdcc83d1a72b0ae5da34bc778b49d8a5606
Henryk Kurze <hk@4so.de>
2017-01-13 11:29:05 +0100

    added stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "extends": "stylelint-config-standard",
+    "ignoreFiles": [
+        "src/sass/vendor/**/*.scss",
+        "src/sass/components/_svg-sprite.scss"
+    ],
+    "plugins": [
+        "stylelint-scss"
+    ],
+    "rules": {
+        // indentation after fourseasons guideline
+        "indentation": [4],
+        // empty lines after fourseasons guideline
+        "declaration-empty-line-before": [
+            "always",
+            {
+                "except": ["after-comment", "after-declaration", "first-nested"],
+                "ignore": ["after-comment", "after-declaration"]
+            }
+        ],
+        // block-no-empty is disabled because sass won't render those anyway
+        "block-no-empty": null
+    }
+};
