95b0be210c3a068be5c0bfe835a3d0c5dfd18479
Theo Salzmann <dev@on3iro.de>
2017-10-16 13:49:46 +0200

    Add post hook to automatically add remote origin

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,8 @@
 import subprocess
+import os
 
 
 subprocess.call(['git', 'init'])
+
+subprocess.call(['git', 'checkout', '-b', 'dev'])
+subprocess.call(['git', 'remote', 'add', 'origin', '{{ cookiecutter.repo_url }}'])
